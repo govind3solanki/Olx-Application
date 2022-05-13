7617b4bc57300982254439f0ffe144a504dbedd8
govind solanki <solankigovind77@gmail.com>
2022-05-13 17:38:27 +0530

    complete

--- many-to-one/src/main/java/com/zensar/ManyToOneApplication.java
+++ many-to-one/src/main/java/com/zensar/ManyToOneApplication.java
@@ -14,7 +14,7 @@ import com.zensar.manytoone.repository.CompanyRepository;
 import com.zensar.manytoone.repository.EmployeeRepository;
 
 @SpringBootApplication
-public class ManyToOneApplication implements CommandLineRunner{
+public class ManyToOneApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManyToOneApplication.class, args);
@@ -22,21 +22,20 @@ public class ManyToOneApplication implements CommandLineRunner{
 
 	@Autowired
 	private EmployeeRepository employeeRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-        Company company=null;
-		
-		List<Employee> list=null;
-		
-		list=new ArrayList<>();
-		
-		company=Company.builder().name("zensar technology").build();
+		Company company = null;
+
+		List<Employee> list = null;
+
+		list = new ArrayList<>();
+
+		company = Company.builder().name("zensar technology").build();
 		list.add(Employee.builder().name("govind").salary(5633.3).company(company).build());
 		list.add(Employee.builder().name("rajesh").salary(5543.3).company(company).build());
 		list.add(Employee.builder().name("rohit").salary(3432.3).company(company).build());
-		
-		
+
 		employeeRepository.saveAll(list);
 	}
 
